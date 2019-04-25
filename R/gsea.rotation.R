e185836c43f10eec7113175bb1e5f530d1f0f19f
Dorum Guro <dogu@ad.irm>
2019-04-25 15:07:05 +0200

    Changing name of t.90th to t90th

--- R/gsea.rotation.R
+++ R/gsea.rotation.R
@@ -1,25 +1,6 @@
-#' GSEA rotation
-#' 
-#' GSEA with rotation test
-#' @param X Gene expression matrix with samples as rows and genes as columns.
-#' @param y Optional vector of 0/1 indicating sample phenotype.
-#' @param S Matrix indicating gene set membership with genes as rows and gene sets as columns. 1 indicates that the gene is a member, 0 indicates not a member.
-#' @param nrot The number of rotations to perform. Default is 500.
-#' @param fun Name of function for computing a gene wise test statistic.
-#' @return  \item{p.values }{A p-value for each gene set}
-#' \item{q.values }{An FDR q-value for each gene set}
-#' @references 
-#' Dorum, G., Snipen, L., Solheim, M. and Sabo (2009) Rotation Testing in Gene Set Enrichment
-#' Analysis for Small Direct Comparison Experiments. \emph{Statistical Applications in Genetics
-#'  and Molecular Biology}, \bold{8}(1), article 34.
-#'  
-#' Subramanian, A., Tamayo, P., Mootha, V. K., Mukherjee, S., Ebert, B. L.,
-#' Gillette, M. A., Paulovich, A., Pomeroy, S. L., Golub, T. R., Lander, E. S.
-#' and Mesirov, J. P (2005) Gene set enrichment analysis: A knowledge-based
-#' approach for interpreting genome-wide expression profiles, \emph{PNAS}, \bold{102},
-#' 15545-15550.
-#' @author Guro Dorum
-#' @export
+
+
+
 gsea.rotation <-
 function(X,y=NULL,S,nrot=500,fun=t90th) {
     
